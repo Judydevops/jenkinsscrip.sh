631128ad64672ecbf7233f908c29fabef840fb28
Yesso Wolou Djele <yessowolou@yahoo.com>
2023-01-29 15:43:14 -0500

    added packages

--- jenkinsscript.sh
+++ jenkinsscript.sh
@@ -14,7 +14,8 @@ systemctl enable docker
 systemctl start docker
 usermod -aG docker ubuntu
 
-# run jenkins
+## Extra packages
+apt-get install vim git -y
 mkdir -p /var/jenkins_home
 chown -R 1000:1000 /var/jenkins_home/
 docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
